8e3b0e2d3bdd1705b122cee83310f10f670ad217
Renan Pereira <renanmol87@gmail.com>
2021-12-03 22:40:43 -0300

    feat(components): create animated button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+  forwardRef
+} from '@chakra-ui/react'
+import { isValidMotionProp, motion, MotionProps } from 'framer-motion'
+
+type AnimatedButtonProps = ChakraButtonProps & MotionProps
+
+type ButtonProps = AnimatedButtonProps & {
+  children: ReactNode
+}
+
+export const Button = ({ children, ...rest }: ButtonProps) => {
+  const MotionButton = motion<ButtonProps>(
+    forwardRef((props, ref) => {
+      const chakraProps = Object.fromEntries(
+        // do not pass framer props to DOM element
+        Object.entries(props).filter(([key]) => !isValidMotionProp(key))
+      )
+      return <ChakraButton ref={ref} {...chakraProps} />
+    })
+  )
+
+  return <MotionButton {...rest}>{children}</MotionButton>
+}
