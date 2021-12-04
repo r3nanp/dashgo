52b0c94e8e2bb8cf897e8fd097c2aa1b43f67d46
Renan Pereira <renanmol87@gmail.com>
2021-12-04 14:27:44 -0300

    feat(components): create active link

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,37 @@
+import { cloneElement, ReactElement } from 'react'
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+
+export type ActiveLinkProps = LinkProps & {
+  children: ReactElement
+  shouldMatchExactHref?: boolean
+}
+
+export const ActiveLink = ({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) => {
+  const { asPath } = useRouter()
+
+  let isActive = false
+
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+    isActive = true
+  }
+
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  ) {
+    isActive = true
+  }
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? 'pink.400' : 'gray.50'
+      })}
+    </Link>
+  )
+}
