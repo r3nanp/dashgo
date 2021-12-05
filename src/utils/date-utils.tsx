6b3ac4f920171a9c19b2fe16238f34d6b26d6872
Renan Pereira <renanmol87@gmail.com>
2021-12-05 16:13:34 -0300

    feat(utils): create date-utils

---
+++ src/utils/date-utils.tsx
@@ -0,0 +1,7 @@
+export const dateFormatter = (date: string | Date) => {
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric'
+  }).format(new Date(date))
+}
